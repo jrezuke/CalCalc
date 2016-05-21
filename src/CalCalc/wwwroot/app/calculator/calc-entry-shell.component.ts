2a38e8971b28be35b38d22340b814833dff8e9c2
jojo2 <jojo2@DELL01>
2016-05-21 19:05:27 -0400

    added styles

--- src/CalCalc/wwwroot/app/calculator/calc-entry-shell.component.ts
+++ src/CalCalc/wwwroot/app/calculator/calc-entry-shell.component.ts
@@ -5,7 +5,8 @@ import { CalcTabsComponent } from './calc-tabs.component'
 @Component({
     selector: 'calc-entry-shell',
     templateUrl: 'app/calculator/calc-entry-shell.component.html',
-    directives: [CalcEntryInfoComponent, CalcTabsComponent]
+    directives: [CalcEntryInfoComponent, CalcTabsComponent],
+    styles: ['#divSubjectInfo{}']
 })
 export class CalcEntryShellComponent implements OnInit {
     constructor() { }
