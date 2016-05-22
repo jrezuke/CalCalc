1d4acf32584104333caea4b7d25cb7d339a59263
jrezuke <j.rezuke@verizon.net>
2016-05-22 16:43:50 -0400

    working on tabs

--- src/CalCalc/wwwroot/app/calculator/calc-tabs.component.ts
+++ src/CalCalc/wwwroot/app/calculator/calc-tabs.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CalculatorTabComponent } from './calc-tab.component'
 @Component({    
     selector: 'calc-tabs',
     templateUrl: '/app/calculator/calc-tabs.component.html'
 })
 export class CalcTabsComponent implements OnInit {
-    public: string activeTab;
+    public activeTab :string;
+    public tabs :CalculatorTabComponent[];
     
     constructor() { }
 
