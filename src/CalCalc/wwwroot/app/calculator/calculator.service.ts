9fcaa624792f116a392c1343999ea0f88adaf305
jojo2 <jojo2@DELL01>
2016-05-24 19:40:29 -0400

    calculator service

--- src/CalCalc/wwwroot/app/calculator/calculator.service.ts
+++ src/CalCalc/wwwroot/app/calculator/calculator.service.ts
@@ -2,3 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { DextroseConcentration } from '../fluid-infusions/dextrose-concentration'
+
+@Injectable()
+export class CalculatorService{
+    
+}
